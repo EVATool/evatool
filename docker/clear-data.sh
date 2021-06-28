8269a048fbb072226f043d6f191b6e089f68450c
Henning Buhl <henning.buhl@yahoo.de>
2021-06-28 22:41:54 +0200

    refactor

---
+++ docker/clear-data.sh
@@ -0,0 +1,2 @@
+// TODO Revert tables to their 0 entry state wihtout changing anything else
+// Delete database and flyway volumes and restart backend? Does that clear all data without chaning anything else?
