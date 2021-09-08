281d3c1c6904e0bd0ddaa26a73d6cea6b72498c0
Henning Buhl <henning.buhl@yahoo.de>
2021-09-08 17:24:56 +0200

    Removed docker integration

--- docker-integration/factory-reset.sh
+++
@@ -1,4 +0,0 @@
-docker-compose down
-docker volume rm evatool_backend_database_volume
-docker volume rm evatool_keycloak_database_volume
-docker-compose up -d
--- docker-integration/update.sh
+++
@@ -1,4 +0,0 @@
-docker-compose down
-docker image rm evatool/evatool-frontend:latest
-docker image rm evatool/evatool-backend:latest
-docker-compose up -d
