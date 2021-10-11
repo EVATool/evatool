84cf2671e55f046e4ee509e9b9ceb52bc2b83984
Henning Buhl <49065678+HenningBuhl@users.noreply.github.com>
2021-10-11 10:58:57 +0200

    Fixed copying of volume data

--- docker/backup.sh
+++ docker/backup.sh
@@ -3,18 +3,18 @@ docker-compose down
 # Create backend database docker volume clone.
 docker volume rm evatool_backend_database_volume_backup
 docker volume create --name evatool_backend_database_volume_backup
-docker container run --rm -it \
+docker container run --rm \
            -v evatool_backend_database_volume:/from \
            -v evatool_backend_database_volume_backup:/to \
-           alpine ash -c "cd /from ; cp -av . /to"
+           alpine ash -c "cd /from ; cp -arv . /to"
 
 # Create keycloak database docker volume clone.
 docker volume rm evatool_keycloak_database_volume_backup
 docker volume create --name evatool_keycloak_database_volume_backup
-docker container run --rm -it \
+docker container run --rm \
            -v evatool_keycloak_database_volume:/from \
            -v evatool_keycloak_database_volume_backup:/to \
-           alpine ash -c "cd /from ; cp -av . /to"
+           alpine ash -c "cd /from ; cp -arv . /to"
 
 # Create local backend docker image clone.
 docker tag evatool/evatool-backend:latest evatool/evatool-backend:backup
--- docker/restore.sh
+++ docker/restore.sh
@@ -3,18 +3,18 @@ docker-compose down
 # Restore from backend database docker volume clone.
 docker volume rm evatool_backend_database_volume
 docker volume create --name evatool_backend_database_volume
-docker container run --rm -it \
+docker container run --rm \
            -v evatool_backend_database_volume_backup:/from \
            -v evatool_backend_database_volume:/to \
-           alpine ash -c "cd /from ; cp -av . /to"
+           alpine ash -c "cd /from ; cp -arv . /to"
 
 # Restore from database docker volume clone.
 docker volume rm evatool_keycloak_database_volume
 docker volume create --name evatool_keycloak_database_volume
-docker container run --rm -it \
+docker container run --rm \
            -v evatool_keycloak_database_volume_backup:/from \
            -v evatool_keycloak_database_volume:/to \
-           alpine ash -c "cd /from ; cp -av . /to"
+           alpine ash -c "cd /from ; cp -arv . /to"
 
 # Start docker-compose with EVATOOL_VERSION overwritten to backend.
 docker-compose up -d
