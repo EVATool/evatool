fe77abc7bbbc21f6503fe1de9439e92a20d19b4d
Henning Buhl <henning.buhl@yahoo.de>
2021-10-12 13:01:30 +0200

    Removed carriage return from variables

--- docker/cert-renew.sh
+++ docker/cert-renew.sh
@@ -1,32 +1,24 @@
 #docker-compose down
 
-
-
+# Extract SERVER_ADDR and SSL_KEYSTORE_PASSWORD from .env file.
 input=".env"
 while IFS= read -r line
 do
   if [[ $line == SERVER_ADDR* ]] ; then
     addr=${line//SERVER_ADDR=/}
+    addr=$(echo $addr | sed -e 's/\r//g')
     echo $addr
   fi
 
   if [[ $line == SSL_KEYSTORE_PASSWORD* ]] ; then
     pw=${line//SSL_KEYSTORE_PASSWORD=/}
+    pw=$(echo $pw | sed -e 's/\r//g')
     echo $pw
   fi
 done < "$input"
 
-
-
-
-
-
 # Re-encrypt certificate for keystore (used by spring backend).
-# TODO
-#cd? /data/certbot/conf/live/$0
-#openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name tomcat -CAfile chain.pem -caname root -password $1
-# TODO how to pass passphrase into command above?
-
-
+cd ./data/certbot/conf/live/$addr
+openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name tomcat -CAfile chain.pem -caname root -password $pw
 
 #docker-compose up -d
